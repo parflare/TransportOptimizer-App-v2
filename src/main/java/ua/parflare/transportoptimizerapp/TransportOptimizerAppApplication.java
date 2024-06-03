36c556bc2053f504037c43a5ac5995e3769600e4
parflare <olexandr2000xd@gmail.com>
2024-06-04 02:25:03 +0300

    init

---
+++ src/main/java/ua/parflare/transportoptimizerapp/TransportOptimizerAppApplication.java
@@ -0,0 +1,13 @@
+package ua.parflare.transportoptimizerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportOptimizerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportOptimizerAppApplication.class, args);
+	}
+
+}
